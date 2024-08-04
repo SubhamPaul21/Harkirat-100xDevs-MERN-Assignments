dfd640bba8e02ab1f65abbdb44009a91078c1611
SubhamPaul21 <subhampaul12@gmail.com>
2024-08-04 15:30:31 +0530

    added logic in app.jsx file to display business card component with props variable passing

--- week-5/business-card/frontend/src/App.jsx
+++ week-5/business-card/frontend/src/App.jsx
@@ -1,11 +1,30 @@
-// import { useState } from 'react'
-import './App.css'
+import { BusinessCard } from './components/BusinessCard'
 
 function App() {
+  const name = "Subham";
+  const description = "Student of Harkirat's 0-100 Cohort 2";
+  const interests = ["React", "JavaScript", "Web3"];
+  const socialMediaLinks = [
+    {
+      "index": 0,
+      "category": "LinkedIn",
+      "link": "https://www.linkedin.com/in/subham-paul-079795142/"
+    },
+    {
+      "index": 1,
+      "category": "Twitter",
+      "link": "https://twitter.com/proofofcode_"
+    },
+    {
+      "index": 2,
+      "category": "Github",
+      "link": "https://github.com/SubhamPaul21/"
+    }
+  ]
 
   return (
     <>
-      <p>Hello World</p>
+      <BusinessCard name={name} description={description} interests={interests} socialMediaLinks={socialMediaLinks}></BusinessCard>
     </>
   )
 }
